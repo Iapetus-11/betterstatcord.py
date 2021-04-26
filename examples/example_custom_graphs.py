a021ffb9499bda88cbb3c5a242857de6680602e4
Milo Weinberg <iapetus011@gmail.com>
2021-04-26 11:45:50 -0400

    add example with custom graphs

---
+++ examples/example_custom_graphs.py
@@ -0,0 +1,20 @@
+from statcord import StatcordClient
+from discord.ext import commands
+
+def custom_graph_1():
+    return 420
+
+def custom_graph_2():
+    return "69"
+
+
+def main():
+    bot = commands.Bot(command_prefix="!")
+
+    bot.statcord_client = StatcordClient(bot, "my statcord key", custom_graph_1, custom_graph_2)
+
+    bot.run("TOKEN")
+
+
+if __name__ == "__main__":
+    main()
