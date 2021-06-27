5d89640f66b82dd560047ff9b4c280d765d0132e
Milo Weinberg <iapetus011@gmail.com>
2021-06-27 13:08:02 -0400

    fix typo in readme

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -1,6 +1,6 @@
 """A better Statcord API library for Python + Discord.py"""
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 from .client import StatcordClient
 from .cluster_client import StatcordClusterClient
