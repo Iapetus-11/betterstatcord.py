e98e97b665cd773eba7815534c181a92e7569d42
Milo Weinberg <iapetus011@gmail.com>
2021-07-13 22:41:19 -0400

    bump version to 3

--- statcord/__init__.py
+++ statcord/__init__.py
@@ -1,6 +1,6 @@
 """A better Statcord API library for Python + Discord.py"""
 
-__version__ = "2.1.0"
+__version__ = "3.0.0"
 
 from .client import StatcordClient
 from .cluster_client import StatcordClusterClient
